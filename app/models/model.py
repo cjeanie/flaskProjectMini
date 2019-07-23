8976de515024ec6ed66c2729c7421541f0efb2a5
EC2 Default User <ec2-user@ip-172-31-18-180.us-east-2.compute.internal>
2019-07-23 19:01:52 +0000

    initial command

---
+++ app/models/model.py
@@ -0,0 +1,5 @@
+def evenOdd(sentence):
+    if len(sentence)%2==0:
+        return "Your sentence has an even number of characters"
+    else:
+        return "Your sentence has an odd number of characters"
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,17 @@ from flask import render_template, request
 from app.models import model, formopener
 
 @app.route('/')
-@app.route('/index')
+# @app.route('/index')
+# def index():
+#     return render_template("/index.html")
+    
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
-    return "hello world!"
+    return render_template ("index.html")
+    
+@app.route('/outputSentence', methods = ['GET', 'POST'])
+def outputSentence():
+    formData = dict(request.form)
+    sentence = formData['sentence'][0]
+    output = model.evenOdd(sentence)
+    return render_template ("outputSentence.html", sentence=sentence, output=output)
